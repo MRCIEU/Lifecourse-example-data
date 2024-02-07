4843bfb853f6917d5c1cdcb71ec651c7af7fcc7e
Gibran Hemani <explodecomputer@gmail.com>
2024-02-07 22:26:20 +0000

    updated

--- scripts/sim_phen.r
+++ scripts/sim_phen.r
@@ -22,7 +22,8 @@ nrep <- 30
 
 bmi <- lapply(1:nid, function(i) {
     tibble(
-        id=i,
+        FID=i,
+        IID=i,
         age=sample(0:80, 30, replace=FALSE),
         value=growth_curve2(age, rnorm(1, mean=23, sd=5), -2.4, rnorm(1, mean=0.3, sd=0.1))
     ) %>% arrange(age)
@@ -30,41 +31,48 @@ bmi <- lapply(1:nid, function(i) {
 
 ldl <- lapply(1:nid, function(i) {
     tibble(
-        id=i,
+        FID=i,
+        IID=i,
         age=sample(0:80, 30, replace=FALSE),
         value=growth_curve2(age, rnorm(1, mean=140, sd=5), -2.4, rnorm(1, mean=1, sd=0.001))
     ) %>% arrange(age)
 }) %>% bind_rows()
 
 covs <- tibble(
-    id = 1:nid,
+    FID = 1:nid,
+    IID = 1:nid,
     yob = runif(nid, 1920, 2020) %>% round(),
     sex = sample(1:2, nid, replace=T),
-    parity = rpois(nid, lambda=1.4)+1,
-    pc1 = rnorm(nid),
-    pc2 = rnorm(nid),
-    pc3 = rnorm(nid),
-    pc4 = rnorm(nid),
-    pc5 = rnorm(nid),
-    pc6 = rnorm(nid),
-    pc7 = rnorm(nid),
-    pc8 = rnorm(nid),
-    pc9 = rnorm(nid),
-    pc10 = rnorm(nid),
-    ancestry = sample(c("EUR", "EAS", "AFR"), nid, replace=T)
+    parity = rpois(nid, lambda=1.4)+1
+)
+
+gencovs <- tibble(
+    FID = 1:nid,
+    IID = 1:nid,
+    PC1 = rnorm(nid),
+    PC2 = rnorm(nid),
+    PC3 = rnorm(nid),
+    PC4 = rnorm(nid),
+    PC5 = rnorm(nid),
+    PC6 = rnorm(nid),
+    PC7 = rnorm(nid),
+    PC8 = rnorm(nid),
+    PC9 = rnorm(nid),
+    PC10 = rnorm(nid)
 )
 
 write.table(bmi, file=here("phenotype", "bmi.txt"), row=F, col=T, qu=F)
 write.table(ldl, file=here("phenotype", "ldl.txt"), row=F, col=T, qu=F)
-write.table(covs, file=here("phenotype", "covs.txt"), row=F, col=T, qu=F)
+write.table(covs, file=here("phenotype", "pheno_covariates.txt"), row=F, col=T, qu=F)
+write.table(gencovs, file=here("phenotype", "geno_covariates.txt"), row=F, col=T, qu=F)
 
 
-ggplot(bmi %>% filter(id < 11), aes(x=age, y=value)) +
-geom_point(aes(colour=as.factor(id))) +
-geom_smooth(aes(colour=as.factor(id)), se=FALSE)
+ggplot(bmi %>% filter(IID < 11), aes(x=age, y=value)) +
+geom_point(aes(colour=as.factor(IID))) +
+geom_smooth(aes(colour=as.factor(IID)), se=FALSE)
 
 
-ggplot(ldl %>% filter(id < 11), aes(x=age, y=value)) +
-geom_point(aes(colour=as.factor(id))) +
-geom_smooth(aes(colour=as.factor(id)), se=FALSE)
+ggplot(ldl %>% filter(IID < 11), aes(x=age, y=value)) +
+geom_point(aes(colour=as.factor(IID))) +
+geom_smooth(aes(colour=as.factor(IID)), se=FALSE)
 
